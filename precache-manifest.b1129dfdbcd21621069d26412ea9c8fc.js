ab35f9e7d07dc09cd18410e88ca1c896ee3eae0e
raveen11 <neupane.rabin11@gmail.com>
2020-12-31 11:01:02 +0545

    Updates

---
+++ precache-manifest.b1129dfdbcd21621069d26412ea9c8fc.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93f9d2fa8da6969e58ab76f834644000",
+    "url": "/raveeenneupane/index.html"
+  },
+  {
+    "revision": "312a1cada598ea74e44f",
+    "url": "/raveeenneupane/static/css/main.6080dd70.chunk.css"
+  },
+  {
+    "revision": "0397937f79a04119f59c",
+    "url": "/raveeenneupane/static/js/2.4fd6d70e.chunk.js"
+  },
+  {
+    "revision": "e3cddcc14d0b349b4e6eccba623e55ae",
+    "url": "/raveeenneupane/static/js/2.4fd6d70e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "312a1cada598ea74e44f",
+    "url": "/raveeenneupane/static/js/main.543511c0.chunk.js"
+  },
+  {
+    "revision": "2aeacf3ca5c149ac2072",
+    "url": "/raveeenneupane/static/js/runtime-main.6352e7ca.js"
+  },
+  {
+    "revision": "bc52ed657704ed430224758b9f5ee944",
+    "url": "/raveeenneupane/static/media/address.bc52ed65.png"
+  },
+  {
+    "revision": "7b92a46af75582358ae99d5775fa5d5a",
+    "url": "/raveeenneupane/static/media/app.7b92a46a.png"
+  },
+  {
+    "revision": "df5a2d73c498714ffada58ef71d76c9b",
+    "url": "/raveeenneupane/static/media/bg1.df5a2d73.jpg"
+  },
+  {
+    "revision": "b79145273ac9e841fb1f90309970b48b",
+    "url": "/raveeenneupane/static/media/cv.b7914527.pdf"
+  },
+  {
+    "revision": "d28d83dc85b72d07428b96dec9bb0953",
+    "url": "/raveeenneupane/static/media/experience.d28d83dc.png"
+  },
+  {
+    "revision": "7ba1aebdab7d8d9e0527daea08ad5683",
+    "url": "/raveeenneupane/static/media/hero.7ba1aebd.jpg"
+  },
+  {
+    "revision": "787f9ee54855a08bcda6a1312d77ab79",
+    "url": "/raveeenneupane/static/media/hero2.787f9ee5.jpg"
+  },
+  {
+    "revision": "1fcabaaeabaf1fe98ffbf6c32a2bb9fb",
+    "url": "/raveeenneupane/static/media/mg.1fcabaae.png"
+  },
+  {
+    "revision": "5b440c14d989000af2276308359709c1",
+    "url": "/raveeenneupane/static/media/phone.5b440c14.png"
+  },
+  {
+    "revision": "e2086d01f45b9e919c9bb8080f9da0b8",
+    "url": "/raveeenneupane/static/media/test.e2086d01.png"
+  },
+  {
+    "revision": "9f139b2187419b3edaf587696fe13d2c",
+    "url": "/raveeenneupane/static/media/vision.9f139b21.png"
+  }
+]);
